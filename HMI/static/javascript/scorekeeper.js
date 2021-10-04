d9568276e5847d846055a22b3afd8d5abad13f4d
BRHouser <brh0033@uah.edu>
2021-10-04 11:14:47 -0500

    js for dart review and add buttons

--- HMI/static/javascript/scorekeeper.js
+++ HMI/static/javascript/scorekeeper.js
@@ -9,6 +9,54 @@ function mouseoverBoard(board_section) {
     $("#score-preview").text(board_section)
 }
 
+// Update displayed stats on scoreboard. new_stats is string representing the stat to be displayed
+function updatePlayerStats(new_stats) {
+    $("#playerStatsDropdown").text(new_stats)
+}
+
+function updateMatchStats(new_stats) {
+    $("#matchStatsDropdown").text(new_stats)
+}
+
+//Functions to add content to throw review tables
+function newTable(leg, turn) {
+    var template = document.querySelector("#throw-header")
+    var clone = template.content.cloneNode(true)
+
+    //add turn header
+    var b = clone.querySelector("b")
+    b.textContent = "Turn " + turn;
+    if(leg != null) {
+        var b = clone.querySelector("h4")
+        b.textContent = "Leg " + leg;
+    }
+
+    var tbody = clone.querySelector("tbody")
+    tbody.id = "tbody" + leg + turn
+
+    document.querySelector("#reviewThrows").appendChild(clone)
+}
+
+function appendTableRow(label, player1, player2, tbody_id) { //tbody_id is "tbody" followed by the leg and turn. ex: "tbody13" for the third throw of first turn
+    var template = document.querySelector("#throw-row")
+    var clone = template.content.cloneNode(true)
+    
+    var th = clone.querySelector("th")
+    th.textContent = label
+
+    var td = clone.querySelectorAll("td");
+    td[0].textContent = player1
+    td[1].textContent = player2
+
+    document.querySelector("#" + tbody_id).appendChild(clone)
+}
+
+function endTableRow() {
+    var template = document.querySelector("#throw-bottom")
+    var clone = template.content.cloneNode(true)
+    document.querySelector("#reviewThrows").appendChild(clone)
+}
+
 function registerImageMap() { //register events for the entire board
     for(let i = 1; i <= 20; i++) {
         registerNum(i)
